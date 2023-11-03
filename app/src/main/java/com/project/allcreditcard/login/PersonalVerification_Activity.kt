5a57c842e6b1a6366546920855c652c4fd260eb6
MyLittleRengar <yjy8860@naver.com>
2023-11-04 00:43:49 +0900

    레이아웃 수정 및 레이아웃과 액티비티코드 연결

---
+++ app/src/main/java/com/project/allcreditcard/login/PersonalVerification_Activity.kt
@@ -0,0 +1,12 @@
+package com.project.allcreditcard.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.allcreditcard.R
+
+class PersonalVerification_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_verification)
+    }
+}
