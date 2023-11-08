c24b12a85bb6619bafc371eee8a996392722c62d
gyunyunee <hgj1811@gmail.com>
2023-11-09 01:44:20 +0900

    오탈자 수정 이용약관, 법률약관 페이지 생성

---
+++ app/src/main/java/com/project/allcreditcard/termsoflaws.kt
@@ -0,0 +1,11 @@
+package com.project.allcreditcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class termsoflaws : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_termsoflaws)
+    }
+}
