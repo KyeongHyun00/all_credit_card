4be87bc38cdfc0872a01fc47bfb540a9f50129c6
MyLittleRengar <yjy8860@naver.com>
2023-11-09 23:54:35 +0900

    액티비티 및 레이아웃 이름 수정

--- app/src/main/java/com/project/allcreditcard/TermsOfLawsActivity.kt
+++ app/src/main/java/com/project/allcreditcard/TermsOfLawsActivity.kt
@@ -3,9 +3,9 @@ package com.project.allcreditcard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class termsoflaws : AppCompatActivity() {
+class TermsOfLawsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_termsoflaws)
+        setContentView(R.layout.activity_terms_of_laws)
     }
 }
--- app/src/main/java/com/project/allcreditcard/TermsOfUseActivity.kt
+++ app/src/main/java/com/project/allcreditcard/TermsOfUseActivity.kt
@@ -3,9 +3,9 @@ package com.project.allcreditcard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class termsofuse : AppCompatActivity() {
+class TermsOfUseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_termsofuse)
+        setContentView(R.layout.activity_terms_of_use)
     }
 }
