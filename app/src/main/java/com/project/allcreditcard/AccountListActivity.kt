62fc932ce9a014adc52dde66cf1e39e62de9378b
MyLittleRengar <yjy8860@naver.com>
2023-11-12 04:09:41 +0900

    액티비티 및 레이아웃 이름 변경, 매니페스트 오류 수정

--- app/src/main/java/com/project/allcreditcard/AccountListActivity.kt
+++ app/src/main/java/com/project/allcreditcard/AccountListActivity.kt
@@ -3,7 +3,7 @@ package com.project.allcreditcard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class account_list : AppCompatActivity() {
+class AccountListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_list)
