46d4cc3362adef60f8321f8448d78ec60c549011
gyunyunee <hgj1811@gmail.com>
2023-11-11 20:16:15 +0900

    결제금액, 신용정보 페이지 생성(만)

---
+++ app/src/main/java/com/project/allcreditcard/CreditInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.project.allcreditcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreditInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit_information)
+    }
+}
